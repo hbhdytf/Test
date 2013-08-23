8e00fb5b57b7966070b27ebbb21ae89e8d23b131
hbhdytf <hbhdyangtengfei@163.com>
2013-08-23 14:32:20 +0800

    TestJNI

---
+++ obj/local/armeabi/objs/TestJni/TestJni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/TestJni/TestJni.o: jni/TestJni.c \
+ G:/Android/ndk/platforms/android-9/arch-arm/usr/include/string.h \
+ G:/Android/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ G:/Android/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ G:/Android/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ G:/Android/ndk/platforms/android-9/arch-arm/usr/include/malloc.h \
+ G:/Android/ndk/platforms/android-9/arch-arm/usr/include/jni.h
+
+G:/Android/ndk/platforms/android-9/arch-arm/usr/include/string.h:
+
+G:/Android/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+G:/Android/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+G:/Android/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+G:/Android/ndk/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+G:/Android/ndk/platforms/android-9/arch-arm/usr/include/jni.h:
